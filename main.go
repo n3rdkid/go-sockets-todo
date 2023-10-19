33b13ffc95a567eb03ab0ce27714e6c4c25439be
Saurav Adhikari <sauravads123@gmail.com>
2023-10-19 09:17:24 +0545

    :sparkles: feat: add socket upgrader and handle function

--- main.go
+++ main.go
@@ -3,12 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var socketUpgrade = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "We did it..... 🎉 ")
 	})
+	http.HandleFunc("/ws", handleWebSocket)
 	fmt.Println("Server has started 🏃")
 	http.ListenAndServe(":8080", nil)
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := socketUpgrade.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Failed to upgrade to socket", http.StatusBadRequest)
+		return
+	}
+	defer conn.Close()
+
 }
